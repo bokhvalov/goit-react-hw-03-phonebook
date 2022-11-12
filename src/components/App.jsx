6bf217d12d1b2e47329b76bf56fefb8bf2e35ca4
Ivan Bokhvalov <bvancha@gmail.com>
2022-11-12 20:39:15 +0100

    saving contacts to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,21 @@ import { Filter } from './ContactList/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts){
+      localStorage.setItem("contacts",JSON.stringify(this.state.contacts))
+    }
+  }
+
+  componentDidMount(){
+    if(localStorage.getItem("contacts")){
+      this.setState({contacts: JSON.parse(localStorage.getItem("contacts"))})
+    }
+  }
 
   submitHandler = newContact => {
     const ContactExist = this.state.contacts.find(
